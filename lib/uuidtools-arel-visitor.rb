cbbb84211888edb02ce847b537f3f22f4f6085b6
Remus Rusanu <contact@rusanu.com>
2014-09-29 15:56:40 -0400

    less file

--- lib/uuidtools-arel-visitor.rb
+++ lib/uuidtools-arel-visitor.rb
@@ -1,3 +1,41 @@
-require 'uuidtools/uuid.rb'
-require 'arel/visitors.rb'
+require 'uuidtools'
+
+# from https://github.com/jashmenn/activeuuid/blob/master/lib/activeuuid/uuid.rb
+
+module Arel
+  module Visitors
+    class DepthFirst < Arel::Visitors::Visitor
+      def visit_UUIDTools_UUID(o, a = nil)
+        o.quoted_id
+      end
+    end
+
+    class MySQL < Arel::Visitors::ToSql
+      def visit_UUIDTools_UUID(o, a = nil)
+        o.quoted_id
+      end
+    end
+
+    class SQLite < Arel::Visitors::ToSql
+      def visit_UUIDTools_UUID(o, a = nil)
+        o.quoted_id
+      end
+    end
+
+    class PostgreSQL < Arel::Visitors::ToSql
+      def visit_UUIDTools_UUID(o, a = nil)
+        "'#{o.to_s}'"
+      end
+    end
+  end
+end
+
+module UUIDTools
+  class UUID
+    def quoted_id
+      s = raw.unpack("H*")[0]
+      "x'#{s}'"
+    end
+  end
+end
 
